6d5b5e95351ccfb39ad8fb9c5989b16e62f8dfaf
Luka Dulčić <luka.dulcic@fer.hr>
2019-05-29 15:46:05 +0200

    Script for recognising faces on picture.
    Former-commit-id: dd88c69c891eaf5f567ad3b16c1f5c56c36a1bc1

---
+++ face_recognition.py
@@ -0,0 +1,46 @@
+from arsfutura_face_recognition.predict import predict
+import argparse
+import base64
+import json
+import cv2
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('Script for recognising faces on picture.')
+    parser.add_argument('--image', required=True, help='Base64 representation of image.')
+    return parser.parse_args()
+
+
+def base64_to_img(bs64_img):
+    decoded = base64.b64decode(bs64_img)
+    return cv2.imdecode(np.frombuffer(decoded, dtype=np.uint8), flags=cv2.IMREAD_COLOR)
+
+
+def img_to_base64(img):
+    ret, buff = cv2.imencode('.png', img)
+    return base64.b64encode(buff)
+
+
+def draw_bb_on_img(faces, img):
+    for face in faces:
+        cv2.rectangle(img, (face.bb.left(), face.bb.top()), (face.bb.right(), face.bb.bottom()), (0, 255, 0), 2)
+        cv2.putText(img, "%s %.2f%%" % (face.identity, face.probability),
+                    (face.bb.left(), face.bb.bottom() + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255), 1)
+
+
+def main():
+    args = parse_args()
+    img = base64_to_img(args.image)
+    faces = predict(img)
+    draw_bb_on_img(faces, img)
+    print json.dumps(
+        {
+            'people': faces,
+            'img': img_to_base64(img)
+        }
+    )
+
+
+if __name__ == '__main__':
+    main()
