8bf837cf48b5b35e82c78da9e1bcf66b1c4e7086
Luka Dulčić <luka.dulcic@fer.hr>
2019-07-29 14:45:20 +0200

    Added concrete arguments in generate_embeddings script.
    Former-commit-id: 71eab79ffde7b81bef84c95ac3d7a1880641c017

--- generate_embeddings.sh
+++ generate_embeddings.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-python -m bin.exif_orientation_normalize --images-path $1
+python -m bin.exif_orientation_normalize --images-path data/images
 echo -e "\n===================================="
 echo "Exif orientation normalization done."
 echo "===================================="
 
-python -m bin.align-mtcnn --input-folder $1 --output-folder $2
+python -m bin.align-mtcnn --input-folder data/images --output-folder data/aligned
 echo -e "\n===================================="
 echo "Cropping faces (aligning) done."
 echo "===================================="
 
-python -m bin.generate_embeddings --input-folder $2 --output-folder $3
+python -m bin.generate_embeddings --input-folder data/aligned --output-folder .
 echo -e "\n===================================="
 echo "Generating face embeddings done."
 echo "===================================="
