dd451b6f64e74826ba07fee0cc41fb1c95e39d01
Luka Dulčić <luka.dulcic@fer.hr>
2019-08-27 13:14:58 +0200

    Text scaling depending on image size in classifier script.
    Former-commit-id: 480027980b1013e79b1778790b0827cc0505f59c

--- inference/util.py
+++ inference/util.py
@@ -3,11 +3,12 @@ from PIL import ImageDraw, ImageFont
 
 def draw_bb_on_img(faces, img):
     draw = ImageDraw.Draw(img)
+    fs = max(20, round(img.size[0] * img.size[1] * 0.000005))
     for face in faces:
         draw.rectangle(((int(face.bb.left), int(face.bb.top)), (int(face.bb.right), int(face.bb.bottom))),
                        outline='green', width=5)
         draw.text(
             (int(face.bb.left), int(face.bb.bottom) + 20),
             "%s %.2f%%" % (face.top_prediction.name.upper(), face.top_prediction.confidence * 100),
-            font=ImageFont.truetype('fonts/font.ttf', 100)
+            font=ImageFont.truetype('fonts/font.ttf', fs)
         )
