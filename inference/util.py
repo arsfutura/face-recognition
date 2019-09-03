5c94f30408387613b4491415caa5cb628e282024
Luka Dulčić <luka.dulcic@fer.hr>
2019-09-03 12:34:33 +0200

    Updated function for drawing bounding boxes and labels.
    Former-commit-id: 22fa154afb877cee741860dd2b6fa0eca52b4942

--- inference/util.py
+++ inference/util.py
@@ -4,11 +4,35 @@ from PIL import ImageDraw, ImageFont
 def draw_bb_on_img(faces, img):
     draw = ImageDraw.Draw(img)
     fs = max(20, round(img.size[0] * img.size[1] * 0.000005))
+    font = ImageFont.truetype('fonts/font.ttf', fs)
+    margin = 10
+
     for face in faces:
-        draw.rectangle(((int(face.bb.left), int(face.bb.top)), (int(face.bb.right), int(face.bb.bottom))),
-                       outline='green', width=5)
+        text = "%s %.2f%%" % (face.top_prediction.label.upper(), face.top_prediction.confidence * 100)
+        text_size = font.getsize(text)
+
+        # bounding box
+        draw.rectangle(
+            (
+                (int(face.bb.left), int(face.bb.top)),
+                (int(face.bb.right), int(face.bb.bottom))
+            ),
+            outline='green',
+            width=5
+        )
+
+        # text background
+        draw.rectangle(
+            (
+                (int(face.bb.left - margin), int(face.bb.bottom) + margin),
+                (int(face.bb.left + text_size[0] + margin), int(face.bb.bottom) + text_size[1] + 3 * margin)
+            ),
+            fill='black'
+        )
+
+        # text
         draw.text(
-            (int(face.bb.left), int(face.bb.bottom) + 20),
-            "%s %.2f%%" % (face.top_prediction.name.upper(), face.top_prediction.confidence * 100),
-            font=ImageFont.truetype('fonts/font.ttf', fs)
+            (int(face.bb.left), int(face.bb.bottom) + 2 * margin),
+            text,
+            font=font
         )
